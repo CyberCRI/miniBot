5eb704da715a4688a74f7276e9098d674b2ed15d
emma <emmamarion.barme@orange.fr>
2018-01-25 17:19:25 +0100

    Bot answer is displayed + history is displayed

--- javascript/talkToMiniBot.js
+++ javascript/talkToMiniBot.js
@@ -1,19 +1,26 @@
 function talkToMiniBot() {
 
   var userInput = document.forms["userInput"]["userInput"].value;
-  console.log(userInput);
   if (userInput == "") {
     alert("Please say something.");
     return false;
   }
 
-  // If the input is valid, request chatbot answer
+  // If the input is valid, update history and request chatbot answer
+  var previousBotMsg = $("#botDiv").html();
+  var userMsgHTML = "<p>" + userInput + "</p>";
+  $("#historyDiv").append("<p><b>Bot:</b></p>", previousBotMsg, "<p><b>You:</b></p>", userMsgHTML);
+  document.forms["userInput"]["userInput"].value = "";
+
+  // HTTP POST request for chatbot answer
   $.ajax({
   type: "POST",
     url: "http://0.0.0.0:8888/minibot/api/msg",
     data: { msg: userInput },
     success: function ( data ) {
-      console.log( data );
+      botMsg = data["msg"];
+      formattedMsg = "<p>" + botMsg + "</p>";
+      $("#botDiv").html(formattedMsg);
     },
     dataType: "json"
   });
