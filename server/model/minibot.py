e49d4759f81c93ce3bd8a04b85736a09f3bc6840
emma <emmamarion.barme@orange.fr>
2018-01-29 11:06:59 +0100

    Potential solve for issue 1: solving path problem

--- server/model/minibot.py
+++ server/model/minibot.py
@@ -27,7 +27,7 @@ with open(intentsPath) as json_data:
 
 # check if model already exists
 appPath = os.path.dirname(os.path.abspath(__file__))
-if not os.path.isfile(os.path.join(appPath, "model/training_data")): # if not create model
+if not os.path.isfile(os.path.join(appPath, "training_data")): # if not create model
     ##### PROCESS DATA #####
     words = []
     classes = []
