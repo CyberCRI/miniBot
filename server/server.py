9f30c5a5fedcc73dbe2eb085c6822ae5a1aaca24
emma <emmamarion.barme@orange.fr>
2018-01-29 11:02:19 +0100

    Potential solve for issue 1: deal with model creation/loading in a unique file

--- server/server.py
+++ server/server.py
@@ -8,14 +8,7 @@ import sys
 appPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.getcwd())
 # If not already created, create model and train it
-# WARNING: For now if creating the model, the server needs to be restarted to use it
-if not os.path.isfile(os.path.join(appPath, "model/training_data")):
-	from model.trainBot import createModel
-	createModel()
-else:
-	# If model already exists, import it
-	import model.queryBot
-
+import model.minibot
 
 # Create the Flask app that will deal with communication and asynchrony
 app = Flask(__name__)
@@ -27,7 +20,7 @@ def get_msg():
 	# Receive message
 	data = request.form.get("msg")
 	# Process it and get answer from bot
-	response = model.queryBot.response(data)
+	response = model.minibot.response(data)
 	# Send answer
 	return jsonify({"msg": response})
 
