29e45f42eca3dbde4acaa7f1725171f7cb93d095
emma <emmamarion.barme@orange.fr>
2018-02-07 11:58:16 +0100

    Add compulsory return to flask method

--- server/server.py
+++ server/server.py
@@ -44,10 +44,10 @@ def complainAboutAnswer():
 	try:
 		logId = lastLogId[clientIP]
 		print(logId)
-		return 1
+		return "OK"
 	except:
 		print("Warning: this user is complaining but has not yet chatted with the bot.")
-		return 0
+		return "Warning: this user is complaining but has not yet chatted with the bot."
 
 # Create route for checking all intents in bot
 @app.route("/minibot/api/intents", methods=["GET", "POST"])
